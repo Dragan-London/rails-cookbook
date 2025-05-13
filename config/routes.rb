696cf6971c45587a4329392597dd1a4f612abad2
Dragan Ajvazovic <204484118+Dragan-London@users.noreply.github.com>
2025-05-13 16:03:38 +0100

    Front end Half way done :)

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
-  resources :categories, except: [:edit, :update, :destroy]
+  root "categories#index"
+  resources :categories, except: [:edit, :update, :destroy] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
